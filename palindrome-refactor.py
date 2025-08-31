36ccd0f38b24e7e0af802ff1a51969a4991d880b
matt-m-111 <mmarks111@hotmail.com>
2025-08-31 11:36:21 -0400

    refactored

--- palindrome-refactor.py
+++ palindrome-refactor.py
@@ -33,24 +33,24 @@ def main():
     def is_palindrome_recursive(s,o):
         if o == 1: 
             working_list = [x.lower() for x in s if x.isalpha()]
-        if o == 2:
+        elif o == 2:
             working_list = [x for x in s if x.isalpha()]
-        if o == 3:
+        elif o == 3:
             working_list = [x.lower() for x in s if x.isalnum()]
-        if o == 4:
+        elif o == 4:
             working_list = [x for x in s if x.isalpha()]
-        if o == 5:
-
-        if o == 6:
-
-        if o == 7:
-        
-        
-        
+        elif o == 5:
+            working_list = [x.lower() for x in s.strip()]
+        elif o == 6:
+            working_list = [x for x in s.strip()]
+        elif o == 7:
+            working_list = [x for x in s]
+        else:
+            raise Exception("something went wrong")
         
         
         if len(working_list) > 2:
-            return is_palindrome_recursive("".join(working_list[1:-1])) if working_list[0] == working_list[-1] else False
+            return is_palindrome_recursive("".join(working_list[1:-1]), o) if working_list[0] == working_list[-1] else False
         return True if ( 
             (len(working_list) == 2 and 
             working_list[0] == working_list[1]) or 
@@ -58,70 +58,7 @@ def main():
             ) else False
 
 
-    # def is_palindrome_recursive_case(s):
-    #     working_list = [x for x in s if x.isalpha()]
-    #     if len(working_list) > 2:
-    #         return is_palindrome_recursive_case("".join(working_list[1:-1])) if working_list[0] == working_list[-1] else False
-    #     return True if ( 
-    #         (len(working_list) == 2 and 
-    #         working_list[0] == working_list[1]) or 
-    #         len(working_list) <= 1
-    #         ) else False
-    
 
-    # def is_palindrome_recursive_num(s):
-    #     working_list = [x.lower() for x in s if x.isalnum()]
-    #     if len(working_list) > 2:
-    #         return is_palindrome_recursive_num("".join(working_list[1:-1])) if working_list[0] == working_list[-1] else False
-    #     return True if ( 
-    #         (len(working_list) == 2 and 
-    #         working_list[0] == working_list[1]) or 
-    #         len(working_list) <= 1
-    #         ) else False
-    
-
-    # def is_palindrome_recursive_num_case(s):
-    #     working_list = [x for x in s if x.isalpha()]
-    #     if len(working_list) > 2:
-    #         return is_palindrome_recursive_num_case("".join(working_list[1:-1])) if working_list[0] == working_list[-1] else False
-    #     return True if ( 
-    #         (len(working_list) == 2 and 
-    #         working_list[0] == working_list[1]) or 
-    #         len(working_list) <= 1
-    #         ) else False
-
-
-    # def is_palindrome_recursive_symb(s):
-    #     working_list = [x.lower() for x in s.strip()]
-    #     if len(working_list) > 2:
-    #         return is_palindrome_recursive_symb("".join(working_list[1:-1])) if working_list[0] == working_list[-1] else False
-    #     return True if ( 
-    #         (len(working_list) == 2 and 
-    #         working_list[0] == working_list[1]) or 
-    #         len(working_list) <= 1
-    #         ) else False
-    
-
-    # def is_palindrome_recursive_symb_case(s):
-    #     working_list = [x for x in s.strip()]
-    #     if len(working_list) > 2:
-    #         return is_palindrome_recursive_symb_case("".join(working_list[1:-1])) if working_list[0] == working_list[-1] else False
-    #     return True if ( 
-    #         (len(working_list) == 2 and 
-    #         working_list[0] == working_list[1]) or 
-    #         len(working_list) <= 1
-    #         ) else False
-    
-
-    # def is_palindrome_recursive_all(s):
-    #     working_list = [x for x in s]
-    #     if len(working_list) > 2:
-    #         return is_palindrome_recursive_all("".join(working_list[1:-1])) if working_list[0] == working_list[-1] else False
-    #     return True if ( 
-    #         (len(working_list) == 2 and 
-    #         working_list[0] == working_list[1]) or 
-    #         len(working_list) <= 1
-    #         ) else False
 
 
 ## User input here
@@ -141,48 +78,19 @@ def main():
         o = get_option()
 
     
-## if block that calls specific functions based on option
+## get the answer
 
-    if o == 1:
-        try:
-            answer = is_palindrome_recursive(s)
-        except Exception as e:
-            print(e)
-    elif o == 2:
-        try:
-            answer = is_palindrome_recursive_case(s)
-        except Exception as e:
-            print(e)
-    elif o == 3:
-        try:
-            answer = is_palindrome_recursive_num(s)
-        except Exception as e:
-            print(e)
-    elif o == 4:
-        try:
-            answer = is_palindrome_recursive_num_case(s)
-        except Exception as e:
-            print(e)
-    elif o == 5:
-        try:
-            answer = is_palindrome_recursive_symb(s)
-        except Exception as e:
-            print(e)
-    elif o == 6:
-        try:
-            answer = is_palindrome_recursive_symb_case(s)
-        except Exception as e:
-            print(e)
-    elif o == 7:
-        try:
-            answer = is_palindrome_recursive_all(s)
-        except Exception as e:
-            print(e)
-    else:
-        print(f'DEBUG INFO = Option chosen = {o}')
-        fail = isinstance(o, int)
-        print(f'DEBUG INFO = Is Option an INT = {fail}')
-        raise Exception("Something went terribly wrong.")
+
+    try:
+        answer = is_palindrome_recursive(s, o)
+    except Exception as e:
+        print(e)
+
+    # else:
+    #     print(f'DEBUG INFO = Option chosen = {o}')
+    #     fail = isinstance(o, int)
+    #     print(f'DEBUG INFO = Is Option an INT = {fail}')
+    #     raise Exception("Something went terribly wrong.")
 
 
 ## fun ascii art
